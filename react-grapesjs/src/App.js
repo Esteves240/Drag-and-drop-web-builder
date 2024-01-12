94e22a91d63e16306d173111eebbe10227ca563c
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2024-01-12 18:33:13 +0000

    Creating State to store editor instance

--- react-grapesjs/src/App.js
+++ react-grapesjs/src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import grapesjs from "grapesjs";
 
 function App() {
+  const [editor, seteditor] = useState(null);
   return (
     <div className="App">
       <div id="editor"></div>
