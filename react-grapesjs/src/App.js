3e74da6613fc354551ec9e6c6352f52f37cc88e2
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2024-01-13 18:52:54 +0000

    useEffect to add GrapesJs configuration

--- react-grapesjs/src/App.js
+++ react-grapesjs/src/App.js
@@ -2,7 +2,14 @@ import React, {useState, useEffect} from "react";
 import grapesjs from "grapesjs";
 
 function App() {
-  const [editor, seteditor] = useState(null);
+  const [editor, setEditor] = useState(null);
+  useEffect(() => {
+    const editor = grapesjs.init({
+      container: "#editor",
+    });
+    setEditor(editor);
+  }, []);
+  
   return (
     <div className="App">
       <div id="editor"></div>
